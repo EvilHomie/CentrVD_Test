4666490564a291f8001739a199b9e0ab38eac835
EvilHomie <132207110+EvilHomie@users.noreply.github.com>
2025-06-17 19:29:18 +0300

    Initial commit

---
+++ FixedEmployeesViewerPlugin/CommandHandler.cs
@@ -0,0 +1,106 @@
+﻿using PhoneApp.Domain.Attributes;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FixedEmployeesViewerPlugin
+{
+    [Author(Name = "Danila Pavlenko")]
+    public class CommandHandler : IPluggable
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+        {
+            logger.Info("Starting Viewer");
+            logger.Info("Type q or quit to exit");
+            logger.Info("Available commands: list, add, del");
+
+            var employeesList = args.Cast<EmployeesDTO>().ToList();
+
+            string command = "";
+
+            while (!command.ToLower().Contains("quit")
+                && !command.ToLower().Contains("q"))
+            {
+                Console.Write("> ");
+                command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "list":
+                        ShowListCommand(employeesList);
+                        break;
+                    case "add":
+                        AddCommand(employeesList);
+                        break;
+                    case "del":
+                        DelCommand(employeesList);
+                        break;
+                }
+
+                Console.WriteLine("");
+            }
+
+            return employeesList.Cast<DataTransferObject>();
+        }
+
+        void ShowListCommand(List<EmployeesDTO> employeesList)
+        {
+            if (employeesList.Count == 0)
+            {
+                Console.Write("List is empty");
+                return;
+            }
+
+            int index = 0;
+            foreach (var employee in employeesList)
+            {
+                Console.WriteLine($"{index} Name: {employee.Name} | Phone: {employee.Phone}");
+                ++index;
+            }
+        }
+
+        void AddCommand(List<EmployeesDTO> employeesList)
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Phone: ");
+            string phone = Console.ReadLine();
+            Console.WriteLine($"{name} added to employees");
+
+            EmployeesDTO newEmployee = new EmployeesDTO()
+            {
+                Name = name,
+                //Phone = phone                //если DTO имеет сеттер в свойсте Phone
+            };
+            newEmployee.AddPhone(phone); //используется в первоначальной версии (когда поля телефонов не заполнялись)
+
+            employeesList.Add(newEmployee);
+        }
+
+        void DelCommand(List<EmployeesDTO> employeesList)
+        {
+            if (employeesList.Count == 0)
+            {
+                Console.Write("Nothing to delete");
+                return;
+            }
+
+            Console.Write("Index of employee to delete: ");
+            int indexToDelete;
+            if (!Int32.TryParse(Console.ReadLine(), out indexToDelete))
+            {
+                logger.Error("Not an index or not an int value!");
+            }
+            else
+            {
+                if (indexToDelete >= 0 && indexToDelete < employeesList.Count())
+                {
+                    employeesList.RemoveAt(indexToDelete);
+                }
+            }
+        }
+    }
+}
---
+++ LoadEmployeePlugin/LoadEmployee.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using PhoneApp.Domain.Attributes;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LoadEmployeePlugin
+{
+    [Author(Name = "Danila Pavlenko")]
+    internal class LoadEmployee : IPluggable
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> input)
+        {
+            logger.Info("Loading employees");
+
+            var employeesList = input.Cast<EmployeesDTO>().ToList();
+
+            var tempList = JsonConvert.DeserializeObject<Dummyjson>(Properties.Resources.dummyjson);
+
+            foreach (var item in tempList.users)
+            {
+                EmployeesDTO newEmployee = new EmployeesDTO()
+                {
+                    Name = $"{item.firstName} {item.lastName}"
+                };
+                newEmployee.AddPhone(item.phone);
+                employeesList.Add(newEmployee);
+
+                /* Версия если исправить DTO, добавив сеттер к свойству Phone. При использовании удалить или закоментировать строки 25-30
+                employeesList.Add(new EmployeesDTO()
+                {
+                    Name = $"{item.firstName} {item.lastName}",
+                    Phone = item.phone
+                });
+                */
+
+            }
+
+            logger.Info($"Loaded {employeesList.Count()} employees");
+            return employeesList.Cast<DataTransferObject>();
+        }
+
+
+        // классы для десирилизации списка размещенного в Resources взятого с https://dummyjson.com/users
+        // помещены внутрь класса т.к. должны использоваться только в нем
+        class Dummyjson
+        {
+            public List<User> users { get; set; }
+        }
+
+        class User
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string phone { get; set; }
+        }
+    }
+}
